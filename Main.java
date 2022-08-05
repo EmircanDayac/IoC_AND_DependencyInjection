bdb333bcced0ef04d42aeb4712123849f4757f11
Emircan Dayac <70799758+emircandayac@users.noreply.github.com>
2022-08-06 01:43:39 +0300

    commit

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
-
+import org.springframework.beans.BeansException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+@Component("x")
 public class Main {
 
 	public static void main(String[] args) {
-	CustomerManager _manager = new CustomerManager(new MySqlDal());
-	_manager.add();
+	try (ClassPathXmlApplicationContext _context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+		//CustomerManager _manager = new CustomerManager(_context.getBean("database",IDal.class));
+		ICustomerService _Service = _context.getBean("service",ICustomerService.class);
+		_Service.add();
+//		IDal _Service_x = _context.getBean("x",IDal.class);
+//		_SERVİCE_X.ADD();
+	} catch (BeansException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
 
+
+//_manager.add(); 
+
+    // class kalıtılmalı yada implement edilmeli yoksa classa bağımlı olunur 
 	}
 	//Ioc bilgisayar programının özel olarak yazılan bölümleri, genel bir çerçeveden denetim akışını alır.
 	//Dependency injection kaba tabir ile bir sınıfın/nesnenin bağımlılıklardan kurtulmasını amaçlayan ve o nesneyi olabildiğince bağımsızlaştıran bir programlama tekniği/prensibidir.
--- MySqlDal.java
+++ MySqlDal.java
@@ -1,9 +1,23 @@
 
+import org.springframework.stereotype.Component;
+@Component("x")
 public class MySqlDal implements IDal {
+
+
+	String connectionString;
+	
+	public String getConnectionString() {
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
 
 	@Override
 	public void add() {
 System.out.println("Mysql bağlandı");		
+System.out.println(this.connectionString);
 	}
 
 }
